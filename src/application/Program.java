f762c8c29b12deae7f4737ea1095d9e6cfdb6328
otavio-sr <osrodrigues47@gmail.com>
2026-01-23 12:27:18 -0300

    implementando delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -41,6 +44,14 @@ public class Program {
 		sellerUpdate.setName("Maria Souza");
 		sellerDao.update(sellerUpdate);
 		System.out.println("Atualização completa");
+		
+		System.out.println("\n=== TEST #6===");
+		System.out.println("Informe o id a ser deletado: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("id deletado com sucesso");
+		
+		sc.close();
 	}
 
 }
